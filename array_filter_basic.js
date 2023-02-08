8093b5bcbaadb6aa39d761bd3634bd9c9cce0901
xXBlackMasterXx <kdi@synoint.com>
2023-02-08 15:30:43 -0500

    Addig support for array filter

--- array_filter_basic.js
+++ array_filter_basic.js
@@ -1,26 +1,26 @@
-function matrix_array_filter(question_code, type, error_message = "Fill out all the rows with a valid response.") {
+function array_filter(filter, question_code, type, required_selections = 1, error_message = "Add your custom message error") {
     // Array with indexes from filtered page
     array_filter = [];
 
     // Iterate over the answers
     response.answers.forEach((answer) => {
         // If responses matches to page and question
-        if(answer.questionCode == question_code){
+        if(answer.questionCode == filter){
             // Save data to array filter
             array_filter.push(Number(answer.code)-1);
         }
     })
 
-    // Get all the rows before filter 
-    original_rows = document.querySelectorAll("table > tbody > tr")
+    // Get all the options before filter 
+    container = document.querySelector("#p_" + question_code)
 
-    // Iterate over all the rows
-    original_rows.forEach((row, index) => {
+    // Iterate over all the options
+    container.querySelectorAll(".form-check").forEach((option, index) => {
         if(typeof(type) == "undefined") {
             // If the row is not included in the array filter
             if(!array_filter.includes(index)){
                 // Remove the row
-                row.remove();
+                option.remove();
             }
         }
 
@@ -29,22 +29,22 @@ function matrix_array_filter(question_code, type, error_message = "Fill out all 
                 // If the row is included in the array filter
                 if(array_filter.includes(index)){
                     // Remove the row
-                    row.remove();
+                    option.remove();
                 }
             }
         }
 
     })
 
-    // Get all the rows after the filter
-    displayed_rows = document.querySelectorAll("table > tbody > tr")
+    // Get all the options after the filter
+    container = document.querySelector("#p_" + question_code)
 
     // Check if next button is pressed
     document.querySelector("#p_next").addEventListener("click", (e) => {
         // Variable to count number of checked buttons
         n_checked = 0;
-        // Get all the buttons in the matrix
-        inputs = document.querySelectorAll(".form-check > input");
+        // Get all the buttons
+        inputs = container.querySelectorAll(".form-check > input");
         // Count the number of checked buttons
         inputs.forEach((input) => {
             if(input.checked == true){
@@ -53,7 +53,7 @@ function matrix_array_filter(question_code, type, error_message = "Fill out all 
         })
 
         // If the number of checked buttons is not equals to the displayed rows
-        if(n_checked != displayed_rows.length) {
+        if(n_checked < required_selections) {
             // Avoid the next button to go to the next page
             e.preventDefault();
             // Show an alert message
@@ -62,4 +62,4 @@ function matrix_array_filter(question_code, type, error_message = "Fill out all 
     })
 }
 
-matrix_array_filter(question_code = "AF1")
+array_filter(filter =  "AF1", question_code = "AF3")
